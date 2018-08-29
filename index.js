ba7e31ea082bfd01074cdf0f55115626d8fe348f
Martynas Kanapinskas <Martis347@gmail.com>
2018-08-30 00:16:21 +0300

    Added support for non-decorator approach.

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import NodeCache from 'node-cache';
 const cache = new NodeCache();
 const autoUpdatedValues = [];
 
-export const cached = ({ cacheTTL = 10, autoUpdate = false } = {}) => (_, propertyKey, descriptor) => {
-  var originalFunction = descriptor.value;
+const handleDecorator = (cacheTTL, autoUpdate, propertyKey, descriptor) => {
+  const originalFunction = descriptor.value;
   descriptor.value = function() {
     const getValueAndAddToCache = () => {
       const result = originalFunction.apply(null, arguments);
@@ -28,6 +28,45 @@ export const cached = ({ cacheTTL = 10, autoUpdate = false } = {}) => (_, proper
   };
 
   return descriptor;
+};
+
+const handleNonDecorator = (cacheTTL, autoUpdate, methodName, target) => {
+  if (!methodName) {
+    throw new Error('Name option is required, when using a non-named function with decorator-cache');
+  }
+  const originalFunction = target;
+
+  const result = function() {
+    const getValueAndAddToCache = () => {
+      const result = originalFunction(...arguments);
+      cache.set(hash, result, cacheTTL);
+      return result;
+    };
+    const hash = ObjectHash({ method: originalFunction.name, args: [...arguments]});
+    let data = cache.get(hash);
+    if (autoUpdate && !autoUpdatedValues.includes(hash)) {
+      setInterval(getValueAndAddToCache, 1000 * cacheTTL);
+      autoUpdatedValues.push(hash);
+    }
+    if (data === undefined) {
+      data = getValueAndAddToCache();
+    }
+
+    return data;
+  }
+
+  return result;
+};
+
+export const cached = ({ cacheTTL = 10, autoUpdate = false, name } = {}) => (target, propertyKey, descriptor) => {
+  let result;
+  if (typeof(target) === 'function') {
+    result = handleNonDecorator(cacheTTL, autoUpdate, name || target.name, target);
+  } else {
+    result = handleDecorator(cacheTTL, autoUpdate, propertyKey, descriptor);
+  }
+
+  return result;
 }
 
 export default {
